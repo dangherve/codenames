2e54d29ccb705562ba5373de6b53fbeac5a59947
dangherve <dangherve@gmail.com>
2026-03-12 13:43:52 -0400

    autorebase tools

---
+++ buildTools/rebase.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+set -e
+git remote add upstream https://github.com/minigolf2000/codenames.git
+git fetch upstream
+git rebase upstream/master
+git remote set-url origin git@github.com:dangherve/codenames.git
+git push --force --set-upstream origin master
